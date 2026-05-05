72b0ac7f673f3f3ed989892fca4fcb97aeae8f54
kunaldawn <kunal.dawn@gmail.com>
2026-05-05 18:19:47 +0530

    the change in KDOS Philosophy, re remove the fat, keep the fat support through layer 3 and make it suck bit less

---
+++ script/phase5.env.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# ██╗  ██╗██████╗  ██████╗ ███████╗
+# ██║ ██╔╝██╔══██╗██╔═══██╗██╔════╝
+# █████╔╝ ██║  ██║██║   ██║███████╗
+# ██╔═██╗ ██║  ██║██║   ██║╚════██║
+# ██║  ██╗██████╔╝╚██████╔╝███████║
+# ╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚══════╝
+# ---------------------------------
+#   KD's Homebrew Linux Distro
+# ---------------------------------
+
+# Environment configuration for KDOS build
+
+export CHROOT=1
+
+export PKG_CONFIG_PATH="/usr/local/share/pkgconfig:/usr/local/lib/pkgconfig:/usr/local/lib64/pkgconfig:/usr/share/pkgconfig:/usr/lib/pkgconfig:/usr/lib64/pkgconfig"${PKG_CONFIG_PATH:+:$PKG_CONFIG_PATH}
+export CFLAGS="-O2 -pipe -std=gnu11 -fPIC"
+export CXXFLAGS="-O2 -pipe -fPIC"
+export LDFLAGS=""
+export MAKEFLAGS="-j12"
+export TERM=dumb
+
+rm -rf /var/cache/kpkg/work
