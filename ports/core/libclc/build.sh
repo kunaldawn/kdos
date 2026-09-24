e551534918bec539141e215f5ea7c81ae69c121d
kunaldawn <kunal.dawn@gmail.com>
2026-09-25 02:33:59 +0530

    mesa build fix in libclc

--- ports/core/libclc/build.sh
+++ ports/core/libclc/build.sh
@@ -25,3 +25,22 @@ for triple in amdgcn-amd-amdhsa-llvm nvptx64-nvidia-cuda \
 	cmake --build build-$triple
 	DESTDIR=$PKG cmake --install build-$triple
 done
+
+# Mesa's rusticl loads spirv-mesa3d-.spv and spirv64-mesa3d-.spv from the
+# libexecdir of libclc.pc; this libclc installs one directory per triple and
+# no pkg-config file, and without both Mesa stops at setup.
+ln -s spirv32-unknown-unknown/libclc.spv "$PKG/usr/share/clc/spirv-mesa3d-.spv"
+ln -s spirv64-unknown-unknown/libclc.spv "$PKG/usr/share/clc/spirv64-mesa3d-.spv"
+install -d "$PKG/usr/share/pkgconfig"
+cat > "$PKG/usr/share/pkgconfig/libclc.pc" <<PC
+prefix=/usr
+datarootdir=\${prefix}/share
+datadir=\${datarootdir}
+pkgdatadir=\${datadir}/clc
+libexecdir=\${pkgdatadir}
+
+Name: libclc
+Description: Library requirements of the OpenCL C programming language
+Version: $version
+PC
+chmod 644 "$PKG/usr/share/pkgconfig/libclc.pc"
